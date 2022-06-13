91c3ea731115511b91777300bbbb2d694ac9176c
DeepaGheewala <deepaparikh18@gmail.com>
2022-06-12 22:01:08 -0500

    modified files

--- Queries/Vine_Review_Analysis.sql
+++ Queries/Vine_Review_Analysis.sql
@@ -11,18 +11,18 @@ FROM review_id_table;
 SELECT *
 FROM vine_table;
 
-IF EXISTS(Vine_total_votes_above20) THEN DROP TABLE Vine_total_votes_above20
+DROP TABLE IF EXISTS Vine_total_votes_above20;
 SELECT * INTO Vine_total_votes_above20
 FROM vine_table
-WHERE total_votes > 20
+WHERE total_votes > 20;
 
-IF EXISTS(Vine_Above50_percent) THEN DROP TABLE Vine_Above50_percent
+DROP TABLE IF EXISTS Vine_Above50_percent;
 SELECT *,
 	CAST(helpful_votes AS FLOAT)/CAST(total_votes AS FLOAT) AS Above50_percent INTO Vine_Above50_percent
 FROM Vine_total_votes_above20
-WHERE CAST(helpful_votes AS FLOAT)/CAST(total_votes AS FLOAT) >=0.5
+WHERE CAST(helpful_votes AS FLOAT)/CAST(total_votes AS FLOAT) >=0.5;
   
-IF EXISTS(Vine_paid_5star) THEN DROP TABLE Vine_paid_5star
+DROP TABLE IF EXISTS Vine_paid_5star;
 SELECT
     (SELECT cast(count(*) AS float)
      FROM Vine_Above50_percent
@@ -34,7 +34,7 @@ SELECT
        AND star_rating = 5) AS "5_star_rating",
      
 	 NULL AS "5_star_percentage" 
-	 INTO Vine_paid_5star
+	 INTO Vine_paid_5star;
 		 
 UPDATE Vine_paid_5star
 SET "5_star_percentage" = CONCAT (
@@ -43,13 +43,15 @@ SET "5_star_percentage" = CONCAT (
 										CAST (("5_star_rating"/total_Paid) * 100 AS Numeric(5, 2))
 										, 2) 
 									AS varchar(10000))
-								,'%')
+								,'%');
 
-SELECT *
-FROM Vine_paid_5star
+SELECT total_paid as "Total Paid",
+       "5_star_rating" as "5* Rating",
+	   "5_star_percentage" as "5* Percentage"
+FROM Vine_paid_5star;
 
 
-IF EXISTS(Vine_unpaid_5star) THEN DROP TABLE Vine_unpaid_5star
+DROP TABLE IF EXISTS Vine_unpaid_5star;
 SELECT
   (SELECT cast(count(*) AS float)
    FROM Vine_Above50_percent
@@ -60,7 +62,7 @@ SELECT
    WHERE vine = 'N'
      AND star_rating = 5) AS "5_star_rating",
    
-   NULL AS "5_star_percentage" INTO Vine_unpaid_5star
+   NULL AS "5_star_percentage" INTO Vine_unpaid_5star;
    
 UPDATE Vine_unpaid_5star
 SET "5_star_percentage" = CONCAT (
@@ -69,6 +71,8 @@ SET "5_star_percentage" = CONCAT (
 										CAST (("5_star_rating"/total_unPaid) * 100 AS Numeric(5, 2))
 										, 2) 
 									AS varchar(10000))
-								,'%')
-SELECT *
-FROM Vine_unpaid_5star
+								,'%');
+SELECT total_unpaid as "Total UnPaid",
+       "5_star_rating" as "5* Rating",
+	   "5_star_percentage" as "5* Percentage"
+FROM Vine_unpaid_5star;
